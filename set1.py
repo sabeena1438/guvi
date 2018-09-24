b95b5eb86cd46c45f5e0db0dfa333b635c814c65
sabeena1438 <43530095+sabeena1438@users.noreply.github.com>
2018-09-24 14:17:32 +0530

    Create set1.py

---
+++ set1.py
@@ -0,0 +1,11 @@
+number = int(input())
+
+# checking the number
+if number < 0:
+	print("negative") 
+elif number > 0:
+    print("positive")
+elif number == 0:
+    print("zero.")
+else:
+    print("The input is not a number")
